5039d7eb5ce600d4fb1df7ca994aa5a932a7e1c9
Paul Szyller <paul.szyller@rackspace.com>
2016-06-19 19:45:42 -0500

    Fixed sanitize function

--- assignment-1/comments.php
+++ assignment-1/comments.php
@@ -3,18 +3,21 @@
 	include('functions.php');
 	session_start();
 
-	//GET data from the form
-	$content = sanitizeString($_POST['content']);
-	$UID = sanitizeString($_POST['UID']);
-	$PID = sanitizeString($_POST['PID']);
-
 	//connect to DB
 	$conn = connect_db();
+
+
+	//GET data from the form
+	$content = sanitizeString($_POST['content'], $conn);
+	$UID = sanitizeString($_POST['UID'], $conn);
+	$PID = sanitizeString($_POST['PID'], $conn);
+
+	//get info on comment owner
 	$result = mysqli_query($conn, "SELECT * FROM users WHERE id = '$UID'");
 	$row = mysqli_fetch_assoc($result);
 
 	//fetch user info
-	$name = sanitizeString($row["Name"]);
+	$name = sanitizeString($row["Name"], $conn);
 
 	//insert into posts database
 	$result_insert = mysqli_query($conn, "INSERT INTO comments (id, PID, comment, UID, name) VALUES (NULL, '$PID', '$content', '$UID', '$name')");
--- assignment-1/signup.php
+++ assignment-1/signup.php
@@ -4,18 +4,21 @@
     //start session
     session_start();
 
+    //connect to db
+    $conn = connect_db();
+
     //get inputs from $_POST
-    $username = sanitizeString($_POST["username"]);
+    $username = sanitizeString($_POST["username"], $conn);
     $password = $_POST["password"];
     $password_conf = $_POST["password_conf"];
-    $name = sanitizeString($_POST["name"]);
-    $email = sanitizeString($_POST["email"]);
-    $dob = sanitizeString($_POST["dob"]);
-    $gender = sanitizeString($_POST["gender"]);
-    $question = sanitizeString($_POST["question"]);
-    $answer = sanitizeString($_POST["answer"]);
-    $location = sanitizeString($_POST["location"]);
-    $profile_pic = sanitizeString($_POST["profile_pic"]);
+    $name = sanitizeString($_POST["name"], $conn);
+    $email = sanitizeString($_POST["email"], $conn);
+    $dob = sanitizeString($_POST["dob"], $conn);
+    $gender = sanitizeString($_POST["gender"], $conn);
+    $question = sanitizeString($_POST["question"], $conn);
+    $answer = sanitizeString($_POST["answer"], $conn);
+    $location = sanitizeString($_POST["location"], $conn);
+    $profile_pic = sanitizeString($_POST["profile_pic"], $conn);
 
     //check for password and confirmation matching
     if($password != $password_conf) {
@@ -25,10 +28,7 @@
     elseif (($hash_pw = password_hash($password, PASSWORD_DEFAULT)) == FALSE){
         echo "Something went wrong, please try again!";
     }
-    $hash_pw = sanitizeString($hash_pw);
-
-    //connect to db
-    $conn = connect_db();
+    $hash_pw = sanitizeString($hash_pw, $conn);
 
     //insert new user into users data table
     $result_insert = mysqli_query($conn, "INSERT INTO `myDB`.`users` (`id`, `Username`, `Password`, `Name`, `email`, `dob`, `gender`, `verification_question`, `verification_answer`, `location`, `profile_pic`) VALUES (NULL, '$username', '$hash_pw', '$name', '$email', '$dob', '$gender', '$question', '$answer', '$location', '$profile_pic')");
