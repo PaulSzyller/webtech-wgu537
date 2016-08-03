2292f9bed0e8a0be92a6daba715558e8b676c675
Paul Szyller <paul.szyller@rackspace.com>
2016-08-02 21:45:49 -0500

    innitial friend view setup

---
+++ assignment-2/fb/app/views/friends.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title')
+    <title>FB | Friend List Page</title>
+@stop
+
+@section('style')
+@stop
+
+@section('content')
+    <div class="container">
+    </div>
+@stop
