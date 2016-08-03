77a5039854a66c2e102dcd41df88a14ba322e1c9
Paul Szyller <paul.szyller@rackspace.com>
2016-08-02 21:24:44 -0500

    added friend migration

---
+++ assignment-2/fb/app/database/migrations/2016_08_03_020721_create_friends_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('friends', function(Blueprint $table)
+		{
+			$table->string('user_id');
+			$table->string('friend_id');
+			$table->string('friend_email');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('friends');
+	}
+
+}
