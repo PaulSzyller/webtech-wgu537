48630f14e0dded17a005c5de9f7bf557f6511383
Paul Szyller <paul.szyller@rackspace.com>
2016-08-02 20:33:00 -0500

    Added assignment 2

---
+++ assignment-2/fb/app/config/services.php
@@ -0,0 +1,31 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Third Party Services
+	|--------------------------------------------------------------------------
+	|
+	| This file is for storing the credentials for third party services such
+	| as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+	| default location for this type of information, allowing packages
+	| to have a conventional place to find your various credentials.
+	|
+	*/
+
+	'mailgun' => array(
+		'domain' => 'https://api.mailgun.net/v3/sandbox5b9dde1f811e42b8b3bbb7e75531976e.mailgun.org',
+		'secret' => 'key-e26db443790836deaeb21d48f26b4167',
+	),
+
+	'mandrill' => array(
+		'secret' => '',
+	),
+
+	'stripe' => array(
+		'model'  => 'User',
+		'secret' => '',
+	),
+
+);
